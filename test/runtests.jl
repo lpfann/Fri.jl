bb32142dd70d15e5d43430bdc6ab1672f7c2859b
Lukas Pfannschmidt <lukas@lpfann.me>
2019-11-27 11:20:11 +0100

    make tests work

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
-using Fri
+using Fri, Test
 using Fri.dataset
 
-X,y = Fri.dataset.generate(100,d_rel=2,d_irrel=2,d_weak=2);
+function test_fit()
 
-bounds = Fri.relevance_bounds(X,y)
-@test len(bounds) == shape(X)[2]
+    X,y = Fri.dataset.generate(40,d_rel=2,d_irrel=2,d_weak=2);
+
+    bounds = Fri.relevance_bounds(X,y)
+    @test size(bounds) == (size(X)[2],2)
+end
+
+test_fit()
